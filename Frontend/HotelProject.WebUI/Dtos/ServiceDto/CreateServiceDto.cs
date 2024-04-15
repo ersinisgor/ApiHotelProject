affdc8ff05e1e7a7576b2b6c800bfac4c105d94d
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-15 13:23:17 +0300

    Implement CRUD operations for Service management in ServiceController

---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/CreateServiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+	public class CreateServiceDto
+	{
+		[Required(ErrorMessage = "Please enter service icon link")]
+		public string ServiceIcon { get; set; }
+		[Required(ErrorMessage = "Please enter service title")]
+		[StringLength(100, ErrorMessage = "Service title must be at least 100 characters")]
+		public string Title { get; set; }
+		public string Description { get; set; }
+	}
+}
---
+++ Frontend/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Dtos.ServiceDto
+{
+	public class UpdateServiceDto
+	{
+		public int ServiceID { get; set; }
+
+		[Required(ErrorMessage = "Please enter service icon link")]
+		public string ServiceIcon { get; set; }
+
+		[Required(ErrorMessage = "Please enter service title")]
+		[StringLength(100, ErrorMessage = "Service title must be at least 100 characters")]
+		public string Title { get; set; }
+
+		[Required(ErrorMessage = "Please enter service description")]
+		[StringLength(500, ErrorMessage = "Service {0} must be at least {1} characters")]
+		public string Description { get; set; }
+	}
+}
