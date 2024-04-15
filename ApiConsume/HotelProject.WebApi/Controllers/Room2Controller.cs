bf24a0613f01107c0e3dc17933501f0777d565d0
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-15 11:44:33 +0300

    Integrate AutoMapper in Room2Controller for room management

---
+++ ApiConsume/HotelProject.WebApi/Controllers/Room2Controller.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DtoLayer.Dtos.RoomDto;
+using HotelProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class Room2Controller : ControllerBase
+	{
+		private readonly IRoomService _roomService;
+		private readonly IMapper _mapper;
+
+		public Room2Controller(IRoomService roomService, IMapper mapper)
+		{
+			_roomService = roomService;
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var rooms = _roomService.TGetList();
+			return Ok(rooms);
+		}
+
+		[HttpPost]
+		public IActionResult AddRoom(AddRoomDto addRoomDto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+			var room = _mapper.Map<Room>(addRoomDto);
+			_roomService.TInsert(room);
+
+			return Ok();
+		}
+
+		[HttpPut]
+		public IActionResult UpdateRoom(UpdateRoomDto updateRoomDto)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest();
+			}
+			var room = _mapper.Map<Room>(updateRoomDto);
+			_roomService.TUpdate(room);
+
+			return Ok("Successfully Updated");
+		}
+	}
+}
