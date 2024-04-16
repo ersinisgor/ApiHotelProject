42c4bc68b7955851a52feb42eafccd8367888eca
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-16 17:43:48 +0300

    Update Room view with dynamic room data

---
+++ Frontend/HotelProject.WebUI/Controllers/RoomController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace HotelProject.WebUI.Controllers
+{
+	public class RoomController : Controller
+	{
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
