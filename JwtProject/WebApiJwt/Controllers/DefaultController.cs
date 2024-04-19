48ca15b6365a39f0ce546b564b1738c988f53d8a
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-19 11:25:03 +0300

    Implement JWT token generation and add controllers for token issuance and role-based access

---
+++ JwtProject/WebApiJwt/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiJwt.Models;
+
+namespace WebApiJwt.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DefaultController : ControllerBase
+	{
+		[HttpGet("[action]")]
+		public IActionResult CreateToken()
+		{
+			return Ok(new CreateToken().TokenCreate());
+		}
+
+		[HttpGet("[action]")]
+		public IActionResult CreateAdminToken()
+		{
+			return Ok(new CreateToken().TokenCreateAdmin());
+		}
+
+		[Authorize]
+		[HttpGet("[action]")]
+		public IActionResult TestAuth()
+		{
+			return Ok("Authorized");
+		}
+
+		[Authorize(Roles ="admin,visitor")]
+		[HttpGet("[action]")]
+		public IActionResult TestAdminRole()
+		{
+			return Ok("Token successfull");
+		}
+	}
+}
---
+++ JwtProject/WebApiJwt/Models/CreateToken.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebApiJwt.Models
+{
+	public class CreateToken
+	{
+		public string TokenCreate()
+		{
+			var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+			SymmetricSecurityKey securityKey = new SymmetricSecurityKey(bytes);
+			SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+			JwtSecurityToken token = new JwtSecurityToken(
+							issuer: "https://localhost",
+							audience: "https://localhost",
+							notBefore: DateTime.Now,
+							expires: DateTime.Now.AddSeconds(20),
+							signingCredentials: signingCredentials
+							);
+
+			JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+			return handler.WriteToken(token);
+		}
+
+		public string TokenCreateAdmin()
+		{
+			var bytes = Encoding.UTF8.GetBytes("aspnetcoreapiapi");
+			SymmetricSecurityKey securityKey = new SymmetricSecurityKey(bytes);
+			SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+			List<Claim> claims = new List<Claim>
+			{
+				new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+				new Claim(ClaimTypes.Role, "admin"),
+				new Claim(ClaimTypes.Role, "visitor")
+			};
+
+			JwtSecurityToken token = new JwtSecurityToken(
+										issuer: "https://localhost",
+										audience: "https://localhost",
+										notBefore: DateTime.Now,
+										expires: DateTime.Now.AddSeconds(20),
+										signingCredentials: signingCredentials,
+										claims: claims
+										);
+
+
+			JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+			return handler.WriteToken(token);
+		}
+
+
+
+	}
+}
