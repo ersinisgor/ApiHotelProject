44e0b8262fff874b61602f4fc21d641348ca4a78
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-19 12:58:53 +0300

    Add Guest Entity Class and SQL Trigger for Customer Count Increase

---
+++ ApiConsume/HotelProject.EntityLayer/Concrete/Guest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+	public class Guest
+	{
+        public int GuestID { get; set; }
+        public String Name { get; set; }
+        public String Surname { get; set; }
+        public String City { get; set; }
+    }
+}
