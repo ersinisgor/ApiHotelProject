1c0bb1874025673d730f1ee7272bf44d73e120a6
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-22 17:13:34 +0300

    Implement SendMessage functionality in Admin Contact Page

--- Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/AdminContactController.cs
@@ -1,14 +1,17 @@
 ﻿using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.SendMessageDto;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.Controllers
 {
-  public class AdminContactController : Controller
-  {
+	public class AdminContactController : Controller
+	{
 		private readonly IHttpClientFactory _httpClientFactory;
 
 		public AdminContactController(IHttpClientFactory httpClientFactory)
@@ -30,8 +33,32 @@ namespace HotelProject.WebUI.Controllers
 			return View();
 		}
 
+		[HttpGet]
+		public IActionResult AddSendMessage()
+		{
+			return View();
+		}
 
-    public PartialViewResult SideBarAdminContactPartial()
+		[HttpPost]
+		public async Task<IActionResult> AddSendMessage(CreateSendMessageDto createSendMessageDto)
+		{
+			createSendMessageDto.SenderMail = "admin@gmail.com";
+			createSendMessageDto.SenderName = "Admin";
+			createSendMessageDto.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createSendMessageDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("http://localhost:11888/api/SendMessage", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("SendBox");
+			}
+
+			return View();
+		}
+
+
+		public PartialViewResult SideBarAdminContactPartial()
 		{
 			return PartialView();
 		}
