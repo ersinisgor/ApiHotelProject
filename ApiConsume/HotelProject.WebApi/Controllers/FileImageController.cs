01154c0563f90e6252bc22be4b2103d228c98075
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-23 22:48:01 +0300

    Add file upload capability to AdminFileController

---
+++ ApiConsume/HotelProject.WebApi/Controllers/FileImageController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class FileImageController : ControllerBase
+  {
+    [HttpPost]
+    public async Task<IActionResult> UploadImage([FromForm] IFormFile file)
+    {
+      if (file == null || file.Length == 0)
+      {
+        return BadRequest("File is empty");
+      }
+
+      var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+      var path = Path.Combine(Directory.GetCurrentDirectory(), "images/" + fileName);
+
+      using (var stream = new FileStream(path, FileMode.Create))
+      {
+        await file.CopyToAsync(stream);
+      }
+
+      return Created("", file);
+    }
+
+
+  }
+}
