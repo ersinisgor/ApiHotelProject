054317cfff175c3f2f56a33111cbb72f911ce578
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-23 20:08:33 +0300

    Implement email sending functionality in AdminMailController

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminMailController.cs
@@ -0,0 +1,53 @@
+﻿using HotelProject.WebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+
+
+namespace HotelProject.WebUI.Controllers
+{
+	public class AdminMailController : Controller
+	{
+	private IConfiguration _configuration;
+
+    public AdminMailController(IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    [HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(AdminMailViewModel model)
+		{
+			MimeMessage mimeMessage = new MimeMessage();
+
+			MailboxAddress mailboxAddressfrom = new MailboxAddress("Hotelier Admin", _configuration["EmailConfig:Email"]);
+			mimeMessage.From.Add(mailboxAddressfrom);
+
+			MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+			mimeMessage.To.Add(mailboxAddressTo);
+
+			var bodyBuilder = new BodyBuilder();
+			bodyBuilder.TextBody = model.Body;
+			mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+			mimeMessage.Subject = model.Subject;
+
+      using (var client = new SmtpClient())
+      {
+        client.Connect("smtp.gmail.com", 587, false);
+        client.Authenticate(_configuration["EmailConfig:Email"], _configuration["EmailConfig:Password"]);
+        client.Send(mimeMessage);
+        client.Disconnect(true);
+      }
+
+      return RedirectToAction("Index");
+    }
+	}
+}
---
+++ Frontend/HotelProject.WebUI/Models/Mail/AdminMailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.WebUI.Models.Mail
+{
+	public class AdminMailViewModel
+	{
+        public string Name { get; set; }
+        public string SenderMail { get; set; }
+        [Required(ErrorMessage ="The Receiver Email must not be empty")]
+        public string ReceiverMail { get; set; }
+        [Required(ErrorMessage = "The Subject must not be empty")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "The Body must not be empty")]
+        public string Body { get; set; }
+    }
+}
