71d91d918ba0c47658436cd99beeac05a8207e72
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-29 20:51:26 +0300

    Implement fetching Twitter and LinkedIn followers - followings using RapidAPI

--- Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardSubscribeCountPartial.cs
@@ -31,9 +31,53 @@ namespace HotelProject.WebUI.ViewComponents.Dashboard
         response.EnsureSuccessStatusCode();
         var body = await response.Content.ReadAsStringAsync();
         ResultInstagramFollowersDto resultInstagramFollowersDtos = JsonConvert.DeserializeObject<ResultInstagramFollowersDto>(body);
-        return View(resultInstagramFollowersDtos);
+        ViewBag.instagramFollowers = resultInstagramFollowersDtos.followers;
+        ViewBag.instagramFollowings = resultInstagramFollowersDtos.following;
+ 
       }
 
-    }
+
+			var client2 = new HttpClient();
+			var request2 = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://twitter32.p.rapidapi.com/getProfile?username=murattyucedag"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "13d2afcfd5mshbadf24aa040719ep12a2adjsn03878d1e4a78" },
+		{ "X-RapidAPI-Host", "twitter32.p.rapidapi.com" },
+	},
+			};
+			using (var response2 = await client2.SendAsync(request2))
+			{
+				response2.EnsureSuccessStatusCode();
+				var body2 = await response2.Content.ReadAsStringAsync();
+				ResultTwitterFollowersDto resultTwitterFollowersDto = JsonConvert.DeserializeObject<ResultTwitterFollowersDto>(body2);
+				ViewBag.twitterFollowers = resultTwitterFollowersDto.data.user_info.followers_count;
+				ViewBag.twitterFollowings = resultTwitterFollowersDto.data.user_info.friends_count;
+			}
+
+
+			var client3 = new HttpClient();
+			var request3 = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile?linkedin_url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fmurat-y%C3%BCceda%C4%9F-186933149%2F&include_skills=false"),
+				Headers =
+	{
+		{ "X-RapidAPI-Key", "13d2afcfd5mshbadf24aa040719ep12a2adjsn03878d1e4a78" },
+		{ "X-RapidAPI-Host", "fresh-linkedin-profile-data.p.rapidapi.com" },
+	},
+			};
+			using (var response3 = await client3.SendAsync(request3))
+			{
+				response3.EnsureSuccessStatusCode();
+				var body3 = await response3.Content.ReadAsStringAsync();
+				ResultLinkedInFollowersDto resultLinkedInFollowersDto = JsonConvert.DeserializeObject<ResultLinkedInFollowersDto>(body3);
+				ViewBag.linkedinFollowers = resultLinkedInFollowersDto.data.followers_count;
+
+			}
+			return View();
+		}
   }
 }
