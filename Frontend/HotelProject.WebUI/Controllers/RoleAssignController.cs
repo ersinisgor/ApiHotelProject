dcf77439f18122c877e163db0a540d65d1a9f1d9
Ersin İşgör <ersinisgor@hotmail.com>
2024-04-30 11:38:34 +0300

    Implement functionality to list users and assign roles via the RoleAssignController. Add views for listing users and assigning roles, enhancing the user management capabilities in the admin panel.

---
+++ Frontend/HotelProject.WebUI/Controllers/RoleAssignController.cs
@@ -0,0 +1,48 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Models.Role;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+	public class RoleAssignController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly RoleManager<AppRole> _roleManager;
+
+		public RoleAssignController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+		{
+			_userManager = userManager;
+			_roleManager = roleManager;
+		}
+
+		public IActionResult Index()
+		{
+			var values = _userManager.Users.ToList();
+			return View(values);
+		}
+
+		public async Task<IActionResult> AssignRole(int id)
+		{
+			var user = _userManager.Users.FirstOrDefault(x => x.Id == id);
+			TempData["userId"] = user.Id;
+			var roles = _roleManager.Roles.ToList();
+			ViewBag.roles = roles;
+			var userRoles = await _userManager.GetRolesAsync(user);
+			List<RoleAssignViewModel> roleAssignViewModels = new List<RoleAssignViewModel>();
+			foreach (var item in roles)
+			{
+				RoleAssignViewModel model = new RoleAssignViewModel();
+				model.RoleID = item.Id;
+				model.RoleName = item.Name;
+				model.RoleExist = userRoles.Contains(item.Name);
+				roleAssignViewModels.Add(model);
+			}
+			return View(roleAssignViewModels);
+		}
+	}
+}
